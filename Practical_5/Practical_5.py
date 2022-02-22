8e55a288cbceab3812bd9f53feec7f132f5cc22c
MeetButani27 <20CS010@charusat.edu.in>
2022-02-22 20:35:28 +0530

    20CS010 - Meet Butani

---
+++ Practical_5/Practical_5.py
@@ -0,0 +1,13 @@
+# 20CS010 - Meet Butani
+# You are given a string and your task is to swap cases. In other words, you need
+# to convert all the lowercase letters to uppercase letters and vice versa.
+# -------------------------------------------------------------------------------
+
+# asking the user to enter a string
+print('Please enter a string: ', end="")
+
+# reading the string from the user
+str = input()
+
+# printing the required swapped string
+print("Required string is:", str.swapcase())
